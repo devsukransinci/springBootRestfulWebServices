4af5b3cb4d55fe48fa56199cef6c3f1cfa36772f
sukrannsn <sukrannsn@outlook.com>
2024-06-16 06:55:31 +0300

    upload

---
+++ src/test/java/com/devsukran/spring_boot_restful_webservices/SpringBootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsukran.spring_boot_restful_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
